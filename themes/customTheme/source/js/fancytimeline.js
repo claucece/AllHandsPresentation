6ee76de97fea71a75693e36c4d225d3314f459ba
Sofia Celi <sceli@thoughtworks.com>
2016-07-18 00:43:21 -0500

    almost everything

---
+++ themes/customTheme/source/js/fancytimeline.js
@@ -0,0 +1,24 @@
+var divs = ["monthstimeline", "monthssecondtimeline", "monthsthridtimeline", "monthsfourthtimeline", "coachingobjectives", "literaturebooks", "personal"];
+var visibleDivId = null;
+
+function toggle_visibility(id) {
+    if (visibleDivId === id) {
+        visibleDivId = null;
+    } else {
+        visibleDivId = id;
+    }
+    hideNonVisibleDivs();
+}
+
+function hideNonVisibleDivs() {
+    var i, id, div;
+    for (i = 0; i < divs.length; i++) {
+        id = divs[i];
+        div = document.getElementById(id);
+        if (visibleDivId === id) {
+            div.style.display = "block";
+        } else {
+            div.style.display = "none";
+        }
+    }
+}
